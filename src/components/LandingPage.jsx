31083d14d6676f11c31e6f0e432be6e74a1e4ef1
Meraj <haquedot@gmail.com>
2024-07-12 23:13:14 +0530

    dHorse

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../assets/styles/LandingPage.css'
+import logo from '../assets/images/logo.svg'
+
+const LandingPage = () => {
+    return (
+        <div className="landing-page">
+            <div className="container">
+                <div className="d-block text-center">
+                    <img src={logo} className='logo' alt="logo" />
+                    <p>Discover</p>
+                    <h1 className='mb-3'>Your Perfect Horse <br/>Ride Experience</h1>
+                    <button type="button">Book Now</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default LandingPage;
